0faf0727e7d99e86061bb8e2c136bed46ea76c73
Morgan Harmon <morgan35@usf.edu>
2022-01-25 17:44:03 -0500

    Commit the change

---
+++ VisualStudio2017-CSharpGitDemoFinalAttempt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2017_CSharpGitDemoFinalAttempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
